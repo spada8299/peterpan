c530cae7f7fc9c3122058923ed3873ca5a05bab5
Spada Kao <jennykao123@gmail.com>
2016-09-04 00:41:52 +0800

    test good haha

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var test = req.db.get('test');
-  test.insert({foo: 'bar'});
   res.render('index', { title: 'Express' });
 });
 
