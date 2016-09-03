d7c4b43408c682a8e36bd3801a9e5caf22c81270
Spada Kao <jennykao123@gmail.com>
2016-09-04 00:22:51 +0800

    test mongodb

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  var test = req.db.get('test');
+	var test = req.db.get('test');
   test.insert({foo: 'bar'});
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
