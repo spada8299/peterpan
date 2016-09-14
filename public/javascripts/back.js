056a89c4b3b524a6280282878e4ce825b6de9200
Spada Kao <jennykao123@gmail.com>
2016-09-15 02:57:30 +0800

    add inset and list function

--- public/javascripts/back.js
+++ public/javascripts/back.js
@@ -1,15 +1,69 @@
 'use strict';
 
 $(document).ready(function() {
+	// page1
 	$('.add').click(function() {
 		$(this).parent().append('<div class="addRow">\
 				<span class="q">序號：</span>\
-				<input type="text" name="num" placeholder="請輸入序號...">\
+				<input type="text" name="num" class="num" placeholder="請輸入序號...">\
 				<span class="cancel">x</span>\
 			</div>');
 		$('.cancel').click(function() {
 			$(this).parent().remove();
 		});
+	});
+
+	$('#numSubmit').click(function() {
+		var allNum = [];
+		$('.num').each(function(index) {
+			console.log(index +': '+ $(this).val());
+			var thisNum = { 'num': $(this).val() };
+			allNum.push(thisNum);
+		});
+		console.log(allNum);
+		console.log(allNum.length);
+
+		$.ajax({
+			data: JSON.stringify({'allNum': allNum}),
+			url: '/users/',
+			method: 'POST',
+			contentType: 'application/json'
+		}).done(function(res) {
+			console.log('ajax msg: '+ res.data);
+			if (res.data === "0") {
+				// err
+				alert("新增失敗，請稍等一會兒後重試一次");
+			} else if (res.data === "1") {
+				// suc
+				alert("新增成功");
+			}
+		});
+	});
+
+	// page2
+	$.ajax({
+		url: '/users/',
+		method: 'GET',
+		contentType: 'application/json'
+	}).done(function(res) {
+		console.log('ajax msg: '+ res.data);
+		console.log(res.content);
+		var con = res.content;
+		for (var i = 0; i < con.length; i++) {
+			if (con[i].email == undefined) {
+				$('#numList').append('<tr rel="'+ con[i]._id +'">\
+						<td>'+ con[i].num +'</td>\
+						<td>not yet</td>\
+						<td> </td>\
+						<td class="mod">修改</td>\
+						<td class="del">刪除</td>\
+					</tr>');
+			}
+		}
+
+		// set event to .mod and .del
+		$('.mod').click();
+		$('.del').click();
 	});
 });
 
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,31 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var db = req.db;
+  var list = db.get('numList');
+  list.find({}, {}, function(e, d) {
+  	if (e != null) {
+  		res.json({data: "0"});
+  	} else {
+  		res.json({data: "1", content: d});
+  	}
+  });
 });
 
 router.post('/', function(req, res, next) {
-	console.log(req.body);
+	var db = req.db;
+	var allNum = req.body.allNum;
+	var list = db.get('numList');
+
+	list.insert(allNum, {}, function(e, d) {
+		if (e != null) {
+			console.log(e);
+			res.json({data: '0'});
+		} else {
+			console.log(d);
+			res.json({data: '1'});
+		}
+	});
 });
 
 module.exports = router;
